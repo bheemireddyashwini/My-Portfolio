043d66189969643bceccdef54b37f34ea807647e
Ashwini-Dci-student <ashwini.bheemireddy@dci-student.org>
2024-11-20 13:07:12 +0100

    final commit

--- src/components/Navbar/Herosection/Bio.js
+++ src/components/Navbar/Herosection/Bio.js
@@ -1,15 +1,15 @@
 const Bio = {
   name: "Ashwini Bheemireddy",
-  roles: ["Full Stack Developer",
+  roles: [
+    "Full Stack Developer",
     "Mobile app developer",
     "Fulent in English",
     "Certified in German (B2 Level) ",
   ],
 
-  
   github: "https://github.com/bheemireddyashwini",
   resume:
-    "https://www.canva.com/design/DAGT9Pmx3n8/jaxj0MNMbY_1NjoCXBdUkg/edit",
+    "https://docs.google.com/document/d/1c8hVdJNiMpT1hsCRwIJWQmotNniqIMWCTmHfxL4LCFc/edit?tab=t.0",
   linkedin: "www.linkedin.com/in/ashwini2127",
 };
 export default Bio;
