8a77d294296757529d7094617e4418d0f36d3374
Ashwini-Dci-student <ashwini.bheemireddy@dci-student.org>
2024-12-05 23:02:08 +0100

    final commit

--- src/components/Navbar/Herosection/Bio.js
+++ src/components/Navbar/Herosection/Bio.js
@@ -9,7 +9,8 @@ const Bio = {
 
   github: "https://github.com/bheemireddyashwini",
   resume:
-    "https://docs.google.com/document/d/1c8hVdJNiMpT1hsCRwIJWQmotNniqIMWCTmHfxL4LCFc/edit?tab=t.0",
+      /* "https://docs.google.com/document/d/1c8hVdJNiMpT1hsCRwIJWQmotNniqIMWCTmHfxL4LCFc/edit?tab=t.0", */
+      "https://www.canva.com/design/DAGX3LfpqvE/5G73YtOewfkvqxS4m_Dfyg/edit",
   linkedin: "www.linkedin.com/in/ashwini2127",
 };
 export default Bio;
