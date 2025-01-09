7af24dc978598984945090b99e9cd31cb9406752
Ashwini-Dci-student <ashwini.bheemireddy@dci-student.org>
2025-01-09 23:06:07 +0100

    modified the data

--- src/components/Navbar/Herosection/Bio.js
+++ src/components/Navbar/Herosection/Bio.js
@@ -4,13 +4,12 @@ const Bio = {
     "Full Stack Developer",
     "Mobile app developer",
     "Fulent in English",
-    "Certified in German (B2 Level)"
+    "Certified in German (B2 Level)",
   ],
 
   github: "https://github.com/bheemireddyashwini",
-  /* resume:
-       */
-      /* "https://www.canva.com/design/DAGX3LfpqvE/5G73YtOewfkvqxS4m_Dfyg/edit", */
-  linkedin: "www.linkedin.com/in/ashwini2127"
+  resume:
+    "https://drive.google.com/file/d/1aIca2Aust_o2PO84zJhG4RVV-pMkC5u7/view?usp=drive_link",
+  linkedin: "www.linkedin.com/in/ashwini2127",
 };
 export default Bio;
