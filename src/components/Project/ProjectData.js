0f9e258a403a3a9309024ba5ccc5bbbf6cdf5e0e
Ashwini-Dci-student <ashwini.bheemireddy@dci-student.org>
2025-03-01 23:28:20 +0100

    final commit

--- src/components/Project/ProjectData.js
+++ src/components/Project/ProjectData.js
@@ -1,6 +1,4 @@
 const ProjectData = [
-
-
   {
     id: 1,
     title: "QuizMaster",
@@ -8,21 +6,20 @@ const ProjectData = [
     image: "/quizmaster.png",
     description:
       "A dataset for a Multiple-Choice Quiz Application featuring diverse questions on topics like geography, science, and history. Ideal for building quiz apps or integrating trivia features into projects.",
-    technologies: [ "HTML","JavaScript", "CSS", ],
+    technologies: ["HTML", "JavaScript", "CSS"],
     category: "javascript",
-    github: "https://github.com/bheemireddyashwini/QuizMaster",  // Placeholder
-    link: "https://quizmaster-98cg.onrender.com/",  // Placeholder
-    
+    github: "https://github.com/bheemireddyashwini/QuizMaster", // Placeholder
+    link: "https://quizmaster-98cg.onrender.com/", // Placeholder
   },
-  
+
   {
     id: 2,
     title: "DiceeChallenge",
-    date: "16 october 2024", 
+    date: "16 october 2024",
     description:
       "A simple and fun dice game built with [your technologies, e.g., HTML, CSS, JavaScript]. Roll the dice, test your luck, and enjoy the thrill of chance! Perfect for learning about randomization, interactive UI, and game mechanics.",
-    image: "/dicee.png", 
-    technologies: ["HTML","JavaScript", "CSS", ],
+    image: "/dicee.png",
+    technologies: ["HTML", "JavaScript", "CSS"],
     category: "javascript",
     github: "https://github.com/bheemireddyashwini/random-dice",
     link: "https://bheemireddyashwini.github.io/random-dice/",
@@ -31,46 +28,31 @@ const ProjectData = [
     id: 3,
     title: "Ecommerce App",
     date: "15 November 2024",
-    image: "/ecommerce.png", 
-    description: "Ecom Express is a modern, responsive e-commerce platform built with React. It offers a seamless shopping experience .",
-    technologies: ["HTML", "JavaScript","React", "CSS",  "State Management",'Tailwind CSS','useReducer'],
-    category: "react",
-    github: "https://github.com/bheemireddyashwini/Ecommerce-website", 
-   link: "https://ecommercesite-kgl9.onrender.com" ,
-  },
-  /* {
-    id: 4,
-    title: "Cloud-Watch",
-    date: "1 December 2024",
-    image: "/cloudwatch.png", 
-    description: "The Weather App is a powerful and user-friendly application that provides current weather updates and forecasts for cities and regions worldwide. Built with React, it showcases real-time data fetching, dynamic UI updates, and responsive design.",
-    technologies: ["React", "JavaScript", "CSS", "HTML", "State Management",'Tailwind CSS','useReducer'],
-    category: "react",
-    github: "https://github.com/bheemireddyashwini/Cloud-Watch", 
-   link: "https://cloud-watch.onrender.com" ,
-  }, */
-
-  /* {
-    id: 4,
-    title: "React Todo List",
-    date: "10 october 2024", 
+    image: "/ecommerce.png",
     description:
-      "A dynamic and user-friendly Todo application built with React. This app allows users to create, update, and delete tasks with ease. Featuring a responsive interface and state management for efficient task handling, it demonstrates core React concepts like components, hooks, and local state.",
-    image: "/todo-list.png", 
-    technologies: ["React", "JavaScript", "CSS", "HTML", "State Management"],
+      "Ecom Express is a modern, responsive e-commerce platform built with React. It offers a seamless shopping experience .",
+    technologies: [
+      "HTML",
+      "JavaScript",
+      "React",
+      "CSS",
+      "State Management",
+      "Tailwind CSS",
+      "useReducer",
+    ],
     category: "react",
-    github: "https://github.com/bheemireddyashwini/Todo-Appilcation.git",
-    link: "https://todo-app-oiwc.onrender.com/",
+    github: "https://github.com/bheemireddyashwini/Ecommerce-website",
+    link: "https://ecommercesite-kgl9.onrender.com",
   },
- */
+
   {
     id: 5,
     title: "Yoga-App",
-    date: "24 december 2024", 
+    date: "24 december 2024",
     description:
       "Tuktuk Yoga: A welcoming space for all, offering accessible yoga classes to build strength, flexibility, and relaxation with experienced instructors.",
-    image: "/tuktuk.png", 
-    technologies: [   "HTML","CSS","JavaScript",  "React","State Management"],
+    image: "/tuktuk.png",
+    technologies: ["HTML", "CSS", "JavaScript", "React", "State Management"],
     category: "react",
     github: "https://github.com/bheemireddyashwini/Tuktuk-yoga",
     link: "https://tuktuk-yoga-9slr.onrender.com/",
@@ -84,10 +66,9 @@ const ProjectData = [
     technologies: ["JavaScript", "React", "CSS", "HTML"],
     category: "react",
     image: "/todo.png",
-   // status: "coming-soon",  // New property to indicate this project is upcoming
-    github: "https://github.com/bheemireddyashwini/Todo_App",  // Placeholder
-    link: "https://todo-app-12od.onrender.com/",  // Placeholder
-  }   
-
+    // status: "coming-soon",  // New property to indicate this project is upcoming
+    github: "https://github.com/bheemireddyashwini/Todo_App", // Placeholder
+    link: "https://todo-app-12od.onrender.com/", // Placeholder
+  },
 ];
 export default ProjectData;
